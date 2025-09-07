913aacf3ca601541922726c40fe8dc4af87a3097
Simona Gudaityte <simona@Simonas-MBP.fritz.box>
2025-09-07 20:00:24 +0200

    he

---
+++ snakehead.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SnakeHeadController : MonoBehaviour
+{
+	public float MoveSpeed = 5f;
+	public float SteerSpeed = 180;
+
+	public int Gap = 8;
+	public float BodySpeed = 5;
+	public GameObject BodyPrefab;
+	public int Points = 0;
+
+	private List<GameObject> BodyParts = new List<GameObject>();
+	private List<Vector3> PositionsHistory = new List<Vector3>();
+
+
+	void Start()
+	{
+		// var snakeLenght = 
+		var snakeLenght = 0;
+		var snakeStartLenght = 3;
+
+		while (snakeLenght < snakeStartLenght)
+		{
+			GrowSnake();
+			snakeLenght++;
+			// if hit wall (update())
+			
+		}
+
+
+		// GrowSnake();
+		// GrowSnake();
+		// GrowSnake();
+		// GrowSnake();
+	}
+	int Update()
+	{
+		//move forward
+		transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+		//steer
+		float steerDirection = Input.GetAxis("Horizontal");
+		transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
+		//store position history
+		PositionsHistory.Insert(0, transform.position);
+		//move body parts
+		int index = 0;
+		foreach (var body in BodyParts)
+		{
+			Vector3 point = PositionsHistory[Mathf.Min(index * Gap, PositionsHistory.Count - 1)];
+			Vector3 moveDirection = point - body.transform.position;
+			body.transform.position += moveDirection * BodySpeed * Time.deltaTime;
+			body.transform.LookAt(point);
+			index++;
+		}
+		return 0;
+	}
+
+	private void GrowSnake()
+	{
+		GameObject body = Instantiate(BodyPrefab);
+		BodyParts.Add(body);
+	}
+	//transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+	//transform.Rotate(0, rotationAngle * Time.deltaTime, 0);
+	//transform.Translate(forward);
+
+	// private void OnCollisionEnter(Collision collision)
+	// {
+	// 	if (collision.gameObject.CompareTag("Wall"))
+	// 	{
+	// 		SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Restart current scene
+	// 	}
+	// }
+
+	// Example: Create a wall at runtime
+// GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+// wall.transform.position = new Vector3(0, 0, 5);
+// wall.tag = "Wall";
+}
+
+
+
